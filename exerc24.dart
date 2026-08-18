3565e3fc629c99db4780df4331153448748b1ed3
Lucas Marangoni <lucas.marangoni2008@gmail.com>
2026-08-18 10:56:05 -0300

    commit

--- exerc24.dart
+++ exerc24.dart
@@ -5,37 +5,155 @@ void main() {
   print('Digite o um valor número:');
   int? num1 = int.parse(stdin.readLineSync() ?? "0");
 
-  String numero1 = 'Um';
-  String numero2 = 'Dois';
-  String numero3 = 'Três';
-  String numero4 = 'Um';
-  String numero5 = 'Um';
-  String numero6 = 'Um';
-  String numero7 = 'Um';
-  String numero8 = 'Um';
-  String numero9 = 'Um';
-  String numero10 = 'Um';
-  String numero11 = 'Um';
-  String numero12 = 'Um';
-  String numero13 = 'Um';
-  String numero14 = 'Um';
-  String numero15 = 'Um';
-  String numero16 = 'Um';
-  String numero17 = 'Um';
-  String numero18 = 'Um';
-  String numero19 = 'Um';
-  String numero20 = 'Um';
+  String numero1 = ' Um';
+  String numero2 = ' Dois';
+  String numero3 = ' Três';
+  String numero4 = ' Quatro';
+  String numero5 = ' Cinco';
+  String numero6 = ' Seis';
+  String numero7 = ' Sete';
+  String numero8 = ' Oito';
+  String numero9 = ' Nove';
+  String numero10 = ' Dez';
+  String numero11 = 'Onze';
+  String numero12 = 'Doze';
+  String numero13 = 'Treze';
+  String numero14 = 'Quartoze';
+  String numero15 = 'Quinze';
+  String numero16 = 'Dezesseis';
+  String numero17 = 'Dezessete';
+  String numero18 = 'Dezoito';
+  String numero19 = 'Dezenove';
+  String numero20 = 'Vinte';
 
+  String numero21 = numero20 + numero1;
 
-  if (num1 > 99){
+  if (num1 > 99) {
     print('Número muito alto, número 99 pra baixo ');
-  }
-  else{
-    
+  } else {
+    if (num1 == 1) {
+      print('${numero1}');
+    }
+
+    if (num1 == 2) {
+      print('${numero2}');
+    }
+    if (num1 == 2) {
+      print('${numero1}');
+    }
+
+    if (num1 == 3) {
+      print('${numero2}');
+    }
+    if (num1 == 3) {
+      print('${numero3}');
+    }
+    if (num1 == 4) {
+      print('${numero4}');
+    }
+    if (num1 == 5) {
+      print('${numero5}');
+    }
+    if (num1 == 6) {
+      print('${numero6}');
+    }
+
+    if (num1 == 7) {
+      print('${numero7}');
+    }
+
+    if (num1 == 8) {
+      print('${numero8}');
+    }
+    if (num1 == 9) {
+      print('${numero9}');
+    }
+    if (num1 == 10) {
+      print('${numero10}');
+    }
+    if (num1 == 11) {
+      print('${numero11}');
+    }
+    if (num1 == 12) {
+      print('${numero12}');
+    }
+
+    if (num1 == 13) {
+      print('${numero13}');
+    }
+    if (num1 == 14) {
+      print('${numero14}');
+    }
+
+    if (num1 == 15) {
+      print('${numero15}');
+    }
+
+    if (num1 == 16) {
+      print('${numero16}');
+    }
+    if (num1 == 17) {
+      print('${numero17}');
+    }
+
+    if (num1 == 18) {
+      print('${numero18}');
+    }
+
+    if (num1 == 19) {
+      print('${numero19}');
+    }
+    if (num1 == 20) {
+      print('${numero20}');
+    }
+
+    if (num1 == 21) {
+      print('${numero20 + numero1}');
+    }
+    if (num1 == 22) {
+      print('${numero20 + numero2}');
+    }
+
+    if (num1 == 23) {
+      print('${numero20 + numero3}');
+    }
+
+    if (num1 == 24) {
+      print('${numero20 + numero4}');
+    }
+
+    if (num1 == 25) {
+      print('${numero20 + numero5}');
+    }
+
+    if (num1 == 26) {
+      print('${numero20 + numero6}');
+    }
+
+    if (num1 == 27) {
+      print('${numero20 + numero7}');
+    }
+    if (num1 == 28) {
+      print('${numero20 + numero8}');
+    }
+
+    if (num1 == 29) {
+      print('${numero20 + numero9}');
+    }
+
+    if (num1 == 30) {
+      print('${numero20 + numero10}');
+    }
+
+    if (num1 == 20) {
+      print('${numero20}');
+    }
+
+    if (num1 == 20) {
+      print('${numero20}');
+    }
+    if (num1 == 20) {
+      print('${numero20}');
+    }
   }
 }
-
-
-  
-  
-
